f36db0969f672001a7d369fa88f8538c8b0e323f
zakhttp <zakhttp@gmail.com>
2016-07-08 23:51:49 +0400

    Add game params and fix typo

--- js/app.js
+++ js/app.js
@@ -3,60 +3,65 @@
  * Canvas bounds
  */
  var bounds = {
-  top: 0,
-  bottom: 0,
-  left: 0,
-  right: 0
+  top: -35,
+  bottom: 380,
+  left: -2,
+  right: 402
 };
 /**
  * Height of a row (wheight of background image unit)
  */
-var rowHeight = 0;
+var rowHeight = 81;
 /**
  * Width of a column (width of background image unit)
  */
-var colWidth = 0;
+var colWidth = 101;
 /**
  * Number of rows of the game canvas
  */
-var numRows = 0;
+var numRows = 6;
 /**
  * Number of columns of the game canvas
  */
-var numCols = 0;
+var numCols = 5;
 /**
  * Player vertical and horizontal steps
  */
 var playerSteps = {
-  x: 0,
-  y: 0
+  x: 101,
+  y: 83
 };
 /**
  * Speed ratio of the enemy. Offers more control to tweak their speed.
  */
-var speedRatio = 0;
+var speedRatio = 10;
+
+var collisionTolerance = {
+  x: 60,
+  y: 30
+};
 
 var Enemy = function(x, y, speed){
-  this. x = x;
-  this.y = y;
+  this.x = x;
+  this.y = (y * rowHeight) + (0.7 * rowHeight);
   this.speed = speed * speedRatio;
   this.sprite = 'images/enemy-bug.png';
 };
 
 Enemy.prototype.update = function(dt){
   this.x += this.speed  * dt;
-  if (this.x > bounds.right){
+  if (this.x > (bounds.right + 100)){
     this.x = bounds.left;
   }
 };
 
 Enemy.prototype.render = function(){
-  ctx.drawImage(Resources.load(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 var Player = function(){
-  this.x = bounds.bottom;
-  this.y = ctx.width/2;
+  this.y = bounds.bottom;
+  this.x = bounds.right/2;
   this.sprite = 'images/char-boy.png';
 };
 
@@ -64,40 +69,48 @@ Player.prototype.update = function(dt){
   if (this.x>bounds.right) { this.x = bounds.right;}
   if (this.x<bounds.left) { this.x = bounds.left;}
   if (this.y>bounds.bottom) { this.y = bounds.bottom;}
-  if (this.y<bounds.top) { this.y = bounds.top;}
+  if (this.y<bounds.top) { this.y = bounds.bottom;}
 };
 
 Player.prototype.handleInputKey = function(keyCode){
   switch(keyCode){
 
     case 'up':
-    this.y -= steps.y;
+    this.y -= playerSteps.y;
     break;
 
     case 'down':
-    this.x += steps.y;
+    this.y += playerSteps.y;
     break;
 
     case 'left':
-    this.x -= steps.x;
+    this.x -= playerSteps.x;
     break;
 
     case 'right':
-    this.x += steps.x;
+    this.x += playerSteps.x;
     break;
   }
 };
 
 Player.prototype.render = function(){
-  ctx.drawImage(Resources.load(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-document.addEvenListener('keyup', function(e){
+var allEnemies = [
+    new Enemy(-100, 1, 12),
+    new Enemy(-800, 2, 11),
+    new Enemy(-700, 0, 15),
+    new Enemy(-300, 2, 9),
+];
+var player = new Player();
+
+document.addEventListener('keyup', function(e){
   var keys = {
     38: 'up',
     40: 'down',
     37: 'left',
     39: 'right'
   };
-  Player.handleInputKey(keys[e.keyCode]);
+  player.handleInputKey(keys[e.keyCode]);
 });
